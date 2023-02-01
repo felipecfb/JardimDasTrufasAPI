1c4043a5048b36c00df17950189ce4bb897b1c24
Felipe Bastos <felipe.bastos.dev@gmail.com>
2023-02-01 17:07:31 -0300

    ✨ feat: criando conexão com o banco de dados

---
+++ src/database/data-source.ts
@@ -0,0 +1,15 @@
+import 'reflect-metadata';
+import "dotenv/config";
+
+import { DataSource } from "typeorm";
+
+export const AppDataSource = new DataSource({
+  type: "mysql",
+  host: process.env.DB_HOST,
+  port: 3306,
+  username: process.env.DB_USER,
+  password: process.env.DB_PWD || "",
+  database: process.env.DB_NAME,
+  entities: [`${__dirname}/**/entities/*.{ts,js}`],
+  migrations: [`${__dirname}/**/migrations/*.{ts,js}`],
+});
--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,22 @@
 import express, { Request, Response } from "express";
+import { AppDataSource } from "./database/data-source";
 
-const PORT = 3333;
+const PORT = process.env.port || 3333;
+
+const HOSTNAME = process.env.HOSTNAME || "http://localhost";
 
 const app = express();
 
-app.get("/", (req: Request, res: Response) => {
-  res.status(200).send("Hello world!")
-});
-
-app.listen(PORT, () => {
-  console.log(`🚀 Servidor rodando com sucesso http://localhost:${PORT}`);
-});
+AppDataSource.initialize().then(() => {
+  app.get("/", (req: Request, res: Response) => {
+    res.status(200).send("Hello world!")
+  });
+  
+  app.listen(PORT, () => {
+    console.log(`🚀 The server was started at: http://${HOSTNAME}:${PORT}`);
+  });
+}).catch(err => {
+  console.log(`Error - ${err}`);
+}).finally(() => {
+  console.log("🎉 Database connected!");
+})
